79365ca89bfe70da152d81130c6bb12d1a295def
Tetz <xenosign@naver.com>
2024-10-18 09:14:41 +0900

    241018 Thymeleaf 템플릿 추가

--- src/main/java/com/tetz/spring_boot_demo/controller/HomeController.java
+++ src/main/java/com/tetz/spring_boot_demo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.tetz.spring_boot_demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("")
-    @ResponseBody
-    public String home() {
-        return "Hello";
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "컨트롤러에서 보낸 메세지 입니다!");
+        return "home";
     }
 }
