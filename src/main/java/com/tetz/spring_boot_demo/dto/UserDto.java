6cfa18ec1e918bfad6aa9b8984ffbdd539ce5eed
Tetz <xenosign@naver.com>
2024-10-22 14:00:19 +0900

    Create UserDto.java

---
+++ src/main/java/com/tetz/spring_boot_demo/dto/UserDto.java
@@ -0,0 +1,77 @@
+package com.tetz.spring_boot_demo.dto;
+
+import java.time.LocalDateTime;
+
+public class UserDto {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    private LocalDateTime createdAt;
+
+    // 기본 생성자
+    public UserDto() {}
+
+    // Getter와 Setter 메서드
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    // toString 메서드
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
