fb98964b1901e25bb9691d20debd4d2913a02cb9
Tetz <xenosign@naver.com>
2024-10-23 15:16:56 +0900

    241023 롬복 적용

--- src/main/java/com/tetz/spring_boot_demo/dto/UserDto.java
+++ src/main/java/com/tetz/spring_boot_demo/dto/UserDto.java
@@ -1,7 +1,14 @@
 package com.tetz.spring_boot_demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private Integer id;
     private String username;
@@ -9,69 +16,4 @@ public class UserDto {
     private String email;
     private String name;
     private LocalDateTime createdAt;
-
-    // 기본 생성자
-    public UserDto() {}
-
-    // Getter와 Setter 메서드
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    // toString 메서드
-    @Override
-    public String toString() {
-        return "UserDto{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", name='" + name + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
-    }
 }
