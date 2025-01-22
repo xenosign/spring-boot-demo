7211592d2aeaabf7cc0883cc17fa7d77b2eaf075
kdtTetz <xenosign@naver.com>
2025-01-22 17:12:10 +0900

    250122 코틀린 적용으로 NPE 방지 예시 코드 추가

--- src/main/java/com/tetz/spring_boot_demo/controller/user/UserController.kt
+++ src/main/java/com/tetz/spring_boot_demo/controller/user/UserController.kt
@@ -1,7 +1,7 @@
 package com.tetz.spring_boot_demo.controller.user
 
 import com.tetz.spring_boot_demo.service.user.UserService
-import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,25 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/user")
-class UserController(private val userService: UserService) {
+class UserController(@Autowired private val userService: UserService) {
 
     @GetMapping("/jdbc")
     fun findAllSingleton(model: Model): String {
-        val users = userService.findAllJdbc()
+        // ?: 연산자를 사용해서 데이터 베이스에서 null 리턴 시, 빈 리스트를 전달하여 런타임 에러 방지
+        val users = userService.findAllJdbc() ?: emptyList()
         model.addAttribute("users", users)
         return "user"
     }
 
     @GetMapping("/mybatis")
     fun findAllMyBatis(model: Model): String {
-        val users = userService.findAllMybatis()
+        val users = userService.findAllMybatis() ?: emptyList()
         model.addAttribute("users", users)
         return "user"
     }
 
     @GetMapping("/jpa")
     fun findAllJpa(model: Model): String {
-        val users = userService.findAllJpa()
+        val users = userService.findAllJpa() ?: emptyList()
         model.addAttribute("users", users)
         return "user"
     }
